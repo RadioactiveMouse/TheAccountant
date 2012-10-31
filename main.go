b1638591670dd872b632c4952674436ec7c3f05d
Tom Townsend <tom.towny@gmail.com>
2012-10-31 14:48:23 +0000

    Implemented basic csv parsing

--- main.go
+++ main.go
@@ -4,21 +4,45 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"encoding/csv"
 )
 
 
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Serve Home")
+	fmt.Println("Served Home")
 }
 
-func import(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(w,"Import endpoint")	
+func importData(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w,"Import endpoint")
+}
+
+//my account
+func account(w http.ResponseWriter, r *http.Request) {
+	log.Print("Account endpoint triggered")
+	scanDir() // change this so it returns the data for vanilla printing
+}
+
+// scanDir should examine the data directory and add the data to the UI
+func scanDir() {
+	file , err := os.Open("data.csv")
+	if err != nil {
+		log.Fatal("Unable to find the data.")
+	}
+	defer file.Close()
+	read := csv.NewReader(file)
+	data, err := read.ReadAll()
+	if err != nil {
+		log.Fatal("Error during csv read")
+	}
+	log.Print(data)
 }
 
 func main() {
-	
+	// do some parsing of the main files
 	http.HandleFunc("/", home)
-	http.HandleFunc("/import", import)
+	http.HandleFunc("/import", importData)
+	http.HandleFunc("/myaccount",account)
 
 	err := http.ListenAndServe("localhost:8000", nil)
 	if err != nil {
